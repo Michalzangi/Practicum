ee54a5868b2d94b481044ee00b8b44829a223b4f
Michal <michal.zangi1@gmail.com>
2024-03-28 05:29:23 +0200

    ים דברים מעודכנים עם עלייה לאוויר עלקs

--- Server.js
+++ Server.js
@@ -1,29 +1,18 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
 const port = 4000;
 
-const { loginUser } = require('../model/mongoDB');
-const { addNewFlight } = require('../model/mongoDB');
-const { getFlight } = require('../model/mongoDB');
-const { updateFlight } = require('../model/mongoDB');
-const { updateAllFlight } = require('../model/mongoDB');
-const { addNewOpinion } = require('../model/mongoDB');
-const { getOpinion } = require('../model/mongoDB');
-const { addNewUser } = require('../model/mongoDB');
-const { deleteExistingUser } = require('../model/mongoDB');
-const { getUser } = require('../model/mongoDB');
-const { addNewCoupon } = require('../model/mongoDB');
-const { getCoupon } = require('../model/mongoDB');
-const { getLastItem } = require('../model/mongoDB');
+// Serve static files from the 'Public\sass' directory
+app.use(express.static(path.join(__dirname, 'Public', 'sass')));
 
-const nodemailer = require('nodemailer');
-
-app.use(express.static('public'));
-app.use(express.json()); 
-
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// Define a route handler for the root URL
+app.get('/', (req, res) => {
+  // You can optionally handle requests to the root URL separately here
+  res.send('Hello from the root URL!');
 });
 
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
