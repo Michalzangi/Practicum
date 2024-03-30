c3577c2f9a88297cfba3d955de7a8acb76bae980
Bar Siboni <barsiboni@gmail.com>
2024-03-30 15:18:20 +0300

    added mongon function and the route from the server.js

--- MongoDB.js
+++ MongoDB.js
@@ -1,23 +1,58 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
+
 const uri = "mongodb+srv://barsiboni:OeTrMMWzNa9cb31W@practicumdatabase.shts5lo.mongodb.net/?retryWrites=true&w=majority&appName=PracticumDatabase";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+// Create a MongoClient instance with options for ServerApiVersion
 const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+    version: '1'
   }
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
-    // Send a ping to confirm a successful connection
+    console.log("Connected to MongoDB");
+
+    // Send a ping to confirm a successful connection (optional)
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
   }
 }
-run().catch(console.dir);
+
+run();
+
+
+async function login(username, password) {
+  try {
+    const database = client.db('Practicum'); // Use the existing client instance
+    const collection = database.collection('Users');
+    const user = await collection.findOne({ UserName: username, Password: password });
+
+    if (user) {
+      if (user.Role === 'Admin') {
+        console.log('Admin logged in');
+        return 1;
+      } else if (user.Role === 'User') {
+        console.log('User logged in');
+        return 2;
+      }
+    } else {
+      console.log('User not found');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    throw new Error('Failed to authenticate user');
+  }
+}
+
+
+
+
+module.exports = { run, login };
--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 4000;
-
+const {login}= require('./MongoDB')
 // Serve static files from the 'Public\sass' directory
 app.use(express.static(path.join(__dirname, 'Public', 'sass')));
 
@@ -15,4 +15,22 @@ app.get('/', (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+app.post('/login', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const loggedIn = await login(username, password);
+
+  if (loggedIn === 1) {
+    // User role is "manager," redirect to manager page
+    res.redirect('/Home.html');
+  } else if (loggedIn === 2) {
+    // User role is "customer," redirect to customer page
+    res.redirect('/HomeManager.html');
+  } else {
+    // The user does not exist, so show an error message.
+    res.send('<script>alert("Try again incorrect Username or Password"); window.location.href = "/index.html";</script>');
+  }
 });
