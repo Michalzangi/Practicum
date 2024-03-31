4a40c92b6287449f1e86abb2a44a51d86c547483
barsiboni <133584001+barsiboni@users.noreply.github.com>
2024-03-31 16:19:02 +0300

    Update Server.js
    Added CostumersAssets

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 4000;
-const {loginUser}= require('./MongoDB')
+const {loginUser, filterAssets}= require('./MongoDB')
 // Serve static files from the 'Public\sass' directoryapp.use(express.static('public'));
 app.use(express.static('public'));
 app.use(express.json()); 
@@ -23,6 +23,22 @@ app.post('/login', async (req, res) => {
     res.status(401).json({ error: error.message });
   }
 });
+
+
+app.get('/CustomerAssets', async (req, res) => {
+  // Retrieve parameters from query string
+  const { assetType, assetPriceMin, assetPriceMax, roomNumber, assetStreetNumber, assetStreet } = req.query;
+
+  try {
+    // Call FilterAssets function with parameters
+    const filteredAssets = await filterAssets(assetType, assetPriceMin, assetPriceMax, assetStreet, assetStreetNumber, roomNumber);
+    res.json(filteredAssets); // Return filtered assets as JSON response
+  } catch (error) {
+    res.status(500).json({ error: error.message }); // Handle any errors
+  }
+});
+
+
 
 
 // Start the server
